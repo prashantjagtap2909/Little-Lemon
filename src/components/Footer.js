bd66af6cc0d8287d35ad938ef71aa9338314fb96
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2024-02-28 20:38:29 +0530

    fixed broken link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -48,21 +48,21 @@ export default function Footer() {
           <h1 className="footer-header">Connect</h1>
           <ul className="footer-links">
             <a
-              href="https://www.facebook.com/thelittlelemonshop/"
+              href="https://www.facebook.com/"
               target="_blank"
               rel="noreferrer"
             >
               Facebook
             </a>
             <a
-              href="https://www.instagram.com/littlelemonmoon/"
+              href="https://www.instagram.com/"
               target="_blank"
               rel="noreferrer"
             >
               Instagram
             </a>
             <a
-              href="https://www.youtube.com/watch?v=3Li-FfypZYE"
+              href="https://www.youtube.com/"
               target="_blank"
               rel="noreferrer"
             >
